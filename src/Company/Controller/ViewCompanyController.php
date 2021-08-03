d1d96681eadb1c73615f29243f56b4961a04794b
YTK <yutakaje@gmail.com>
2021-08-03 19:06:05 +0200

    Add controller for viewing a company

---
+++ src/Company/Controller/ViewCompanyController.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the stg/hall-of-records package.
+ *
+ * (c) YTK <yutakaje@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Stg\HallOfRecords\Company\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Stg\HallOfRecords\Company\Application\Query\ViewCompanyQueryHandlerInterface;
+use Stg\HallOfRecords\Company\Template\ViewCompanyTemplateInterface;
+use Stg\HallOfRecords\Shared\Application\Query\ViewQueryCreator;
+use Stg\HallOfRecords\Shared\Infrastructure\Utils\Validator;
+
+final class ViewCompanyController
+{
+    private ViewCompanyQueryHandlerInterface $queryHandler;
+    private ViewCompanyTemplateInterface $template;
+    private ViewQueryCreator $queryCreator;
+    private LoggerInterface $logger;
+
+    public function __construct(
+        ViewCompanyQueryHandlerInterface $queryHandler,
+        ViewCompanyTemplateInterface $template,
+        ViewQueryCreator $queryCreator,
+        LoggerInterface $logger
+    ) {
+        $this->queryHandler = $queryHandler;
+        $this->template = $template;
+        $this->queryCreator = $queryCreator;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param QueryParams $args
+     */
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args = []
+    ): ResponseInterface {
+        $id = Validator::id($args['id'] ?? null);
+
+        $result = $this->queryHandler->execute(
+            $this->queryCreator->create($id, $request)
+        );
+
+        $this->logger->info('Company viewed.', [
+            'company_id' => $id,
+        ]);
+
+        return $this->template->respond($response, $result);
+    }
+}
